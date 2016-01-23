19b2b568682ad92f9a8536fe6b4408f1cbef5425
GeorgeSalu <george.salu10@gmail.com>
2016-01-23 18:44:03 -0200

    Condições de Validação

--- ola-mundo/src/br/edu/olamundo/servlet/Formulario.java
+++ ola-mundo/src/br/edu/olamundo/servlet/Formulario.java
@@ -19,9 +19,41 @@ public class Formulario extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/servlet-resultado.jsp");
-		dispatcher.forward(request	, response);
+		String redirect = "jsp/servlet-examplo.jsp";
+		if (validarCamposObg(request, response)) {
+			redirect = "jsp/servlet-resultado.jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(redirect);
+		dispatcher.forward(request, response);
 	}
 	
+	
+	private boolean validarCamposObg(HttpServletRequest request, HttpServletResponse response) {
+		boolean retorno = true;
+		String msgErro = null;
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String cpf = request.getParameter("cpf");
+		String nasc = request.getParameter("nasc");
+		
+		if (nome == null) {
+			retorno = false;
+			msgErro = "Campo Nome obrigatório!";
+		} else if (endereco == null) {
+			retorno = false;
+			msgErro = "Campo Endereço obrigatório!";
+		} else if (cpf == null) {
+			retorno = false;
+			msgErro = "Campo CPF obrigatório!";
+		} else if (nasc == null) {
+			retorno = false;
+			msgErro = "Campo Dt. Nasc obrigatório!";
+		}
+		request.setAttribute("msgErro", msgErro);
+		return retorno;
+	}
+
+
 	
 }
