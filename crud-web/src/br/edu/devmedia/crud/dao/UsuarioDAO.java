77ae32c2b6dce544d7f82d060f89f4809309a10c
GeorgeSalu <george.salu10@gmail.com>
2016-02-14 14:24:16 -0200

    Explorando o padrão DAO

---
+++ crud-web/src/br/edu/devmedia/crud/dao/UsuarioDAO.java
@@ -0,0 +1,16 @@
+package br.edu.devmedia.crud.dao;
+
+/**
+ * Classe DAO de acesso a dados referentes às entidades de tipo Usuário.
+ * 
+ * @author George
+ * 
+ */
+public class UsuarioDAO {
+
+	public boolean validarUsuario(String usuario, String senha) {
+		
+		return false;
+	}
+	
+}
