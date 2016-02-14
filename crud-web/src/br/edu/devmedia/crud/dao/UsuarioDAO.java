06b001359b78dd32d88d27eee88129616fd6bf3b
GeorgeSalu <george.salu10@gmail.com>
2016-02-14 14:55:33 -0200

    Autenticando usuários no Banco

--- crud-web/src/br/edu/devmedia/crud/dao/UsuarioDAO.java
+++ crud-web/src/br/edu/devmedia/crud/dao/UsuarioDAO.java
@@ -1,5 +1,15 @@
 package br.edu.devmedia.crud.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.edu.devmedia.crud.dto.UsuarioDTO;
+import br.edu.devmedia.crud.exception.PersistenciaException;
+import br.edu.devmedia.crud.util.ConexaoUtil;
+
+
 /**
  * Classe DAO de acesso a dados referentes às entidades de tipo Usuário.
  * 
@@ -8,9 +18,31 @@ package br.edu.devmedia.crud.dao;
  */
 public class UsuarioDAO {
 
-	public boolean validarUsuario(String usuario, String senha) {
-		
-		return false;
+	/**
+	 * Método de autenticação do usuário
+	 * 
+	 * @param usuarioDTO
+	 * @return
+	 * @throws PersistenciaException
+	 */
+	public boolean validarUsuario(UsuarioDTO usuarioDTO) throws PersistenciaException {
+		try {
+			Connection conexao = ConexaoUtil.getConexao();
+			
+			StringBuilder sql = new StringBuilder();
+			sql.append("SELECT * FROM TB_USUARIO ");
+			sql.append(" WHERE USUARIO = ? AND SENHA = ?");
+			
+			PreparedStatement statement = conexao.prepareStatement(sql.toString());
+			statement.setString(1, usuarioDTO.getUsuario());
+			statement.setString(2, usuarioDTO.getSenha());
+			
+			ResultSet resultSet = statement.executeQuery();
+			return resultSet.next();
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new PersistenciaException(e);
+		}
 	}
+
 	
 }
