a84438c7199b4aa76441263fabd256a26a324437
GeorgeSalu <george.salu10@gmail.com>
2016-02-20 15:53:37 -0200

    HttpServletRequest: Validando dados do login

--- crud-web/src/br/edu/devmedia/crud/bo/UsuarioBO.java
+++ crud-web/src/br/edu/devmedia/crud/bo/UsuarioBO.java
@@ -1,11 +1,16 @@
 package br.edu.devmedia.crud.bo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import br.edu.devmedia.crud.dao.UsuarioDAO;
 import br.edu.devmedia.crud.dto.UsuarioDTO;
 import br.edu.devmedia.crud.exception.NegocioException;
-import br.edu.devmedia.crud.exception.PersistenciaException;
+import br.edu.devmedia.crud.exception.ValidationException;
+import br.edu.devmedia.crud.util.Util;
+import br.edu.devmedia.crud.validator.LoginValidator;
 
 /**
  * Classe responsável por gerenciar os métodos de negócio do usuário
@@ -32,8 +37,19 @@ public class UsuarioBO {
 			usuarioDTO.setUsuario(usuario);
 			usuarioDTO.setSenha(senha);
 
+			// Valida campos obg
+			Map<String, Object> valores = new HashMap<>();
+			valores.put("Usuário", usuario);
+			valores.put("Senha", senha);
+			if (new LoginValidator().validar(valores)) {
+				isValido = true;
+			}
+			
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
 			isValido = usuarioDAO.validarUsuario(usuarioDTO);
+		} catch (ValidationException e) {
+			request.setAttribute("msgErro", Util.concatenaMensagensRequest(request, e, "msgErro"));
+			isValido = false;
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new NegocioException(e);
---
+++ crud-web/src/br/edu/devmedia/crud/util/MensagemContantes.java
@@ -0,0 +1,9 @@
+package br.edu.devmedia.crud.util;
+
+public class MensagemContantes {
+
+	public static final String MSG_ERR_CAMPO_OBRIGATORIO = "Campo ? obrigatório!";
+	
+	public static final String MSG_ERR_CAMPO_INVALIDO = "Campo ? inválido!";
+	
+}
